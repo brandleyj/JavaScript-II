d68089ac6d73d9c19862dcb7f6312f200a9fb2fa
Justin Brandley <brandleyj@gmail.com>
2019-08-20 17:22:16 -0500

    MVP

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -38,32 +38,55 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+	// getLength passes the length of the array into the callback.
+	return cb(arr.length);
 }
+getLength(items, function(arrLength) {
+	console.log(arrLength);
+});
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+	// last passes the last item of the array into the callback.
+	return cb(arr[arr.length - 1]);
 }
+last(items, function(arrLast) {
+	console.log(arrLast);
+});
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+	// sumNums adds two numbers (x, y) and passes the result to the callback.
+	return cb(x, y);
 }
+sumNums(45, 13, function(x, y) {
+	console.log(x + y);
+});
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+	// multiplyNums multiplies two numbers and passes the result to the callback.
+	return cb(x, y);
 }
+multiplyNums(23, 6, function(x, y) {
+	console.log(x * y);
+});
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+	// contains checks if an item is present inside of the given array/list.
+	// Pass true to the callback if it is, otherwise pass false.
+	if (list.includes(item)) {
+		return cb(true);
+	} else {
+		return cb(false);
+	}
 }
+contains("Beer", items, function(result) {
+	console.log(result);
+});
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+	// removeDuplicates removes all duplicate values from the given array.
+	// Pass the duplicate free array to the callback function.
+	// Do not mutate the original array.
 }
